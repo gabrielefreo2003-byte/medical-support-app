52691227424c3c2971c01000cb14ae2f4db23846
gabrielefreo2003-byte <gabrielefreo2003@gmail.com>
2026-02-24 15:58:48 +0000

    Update

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ if state and "dateClick" in state:
     s_schiena = st.sidebar.checkbox("Mal di Schiena")
     s_diarrea = st.sidebar.checkbox("Diarrea")
     s_stipsi = st.sidebar.checkbox("Stipsi")
-    s_nausea = st.sidebar.checkbox("Nausea")
     s_alterazioni_cutanee = st.sidebar.checkbox("Alterazioni cutanee")
     s_angina = st.sidebar.checkbox("Angina")
     s_ansia = st.sidebar.checkbox("Ansia")
